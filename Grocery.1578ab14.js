200715fd39f4440382dc232f84cf48eec823bd2e
Sangeetha priya <sangeethalaxmi18@gmail.com>
2025-04-27 16:50:43 +0530

    Updates

---
+++ Grocery.1578ab14.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire041a;(0,e.register)("loR3J",function(r,i){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var a=e("ayMG0"),l=()=>(0,a.jsx)("div",{children:(0,a.jsx)("h1",{children:"it hase many files"})})});
+//# sourceMappingURL=Grocery.1578ab14.js.map
